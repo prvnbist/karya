e67879ba697e9812e07ab72a5cffc625c0f70db9
Praveen Bisht <prvnbist@gmail.com>
2019-12-14 17:00:32 +0530

    fix(warning): deprecation

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,20 @@
-const { ApolloServer } = require("apollo-server");
-const mongoose = require("mongoose");
+const { ApolloServer } = require('apollo-server')
+const mongoose = require('mongoose')
 
-const schema = require("./schema");
+const schema = require('./schema')
 
 mongoose
-  .connect("mongodb://localhost:27017/todo-app", {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(console.log("💾 Connected to DB"))
-  .catch(err => console.error(err));
+   .connect('mongodb://localhost:27017/todo-app', {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+   })
+   .then(console.log('💾 Connected to DB'))
+   .catch(err => console.error(err))
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ schema })
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+   console.log(`🚀  Server ready at ${url}`)
+})
