5b1de535b4e0630e92a69ad194423355f040f6ff
Praveen Bisht <prvnbist@gmail.com>
2019-12-14 23:06:40 +0530

    feat(apollo): setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloClient } from 'apollo-client'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
-const App = () => <div>Hello World</div>
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const link = new HttpLink({
+   uri: 'http://localhost:4000/',
+})
+
+const client = new ApolloClient({
+   link,
+   cache: new InMemoryCache(),
+})
+
+ReactDOM.render(
+   <ApolloProvider client={client}>
+      <App />
+   </ApolloProvider>,
+   document.getElementById('root')
+)
