b0b97b77d95d3d3758f442ffa07b41e32e2637b2
Praveen Bisht <prvnbist@gmail.com>
2019-12-14 23:44:23 +0530

    styles: styled-components

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import {
    InMemoryCache,
    IntrospectionFragmentMatcher,
 } from 'apollo-cache-inmemory'
+import { createGlobalStyle } from 'styled-components'
 
 import App from './App'
 
@@ -27,8 +28,22 @@ const client = new ApolloClient({
    cache: new InMemoryCache({ fragmentMatcher }),
 })
 
+const GlobalStyle = createGlobalStyle`
+   @import url('https://fonts.googleapis.com/css?family=IBM+Plex+Sans:200,300,400,500,600&display=swap');
+   * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      font-family: 'IBM Plex Sans', sans-serif;
+   }
+   body {
+      font-family: 'IBM Plex Sans', sans-serif;
+   }
+`
+
 ReactDOM.render(
    <ApolloProvider client={client}>
+      <GlobalStyle />
       <App />
    </ApolloProvider>,
    document.getElementById('root')
