8db81282aba6c8ab0bbc5b01b71a95e27ca7d6f5
Praveen Bisht <prvnbist@gmail.com>
2019-12-14 16:30:24 +0530

    feat(types): Success/Error union

--- server/src/schema/types/index.js
+++ server/src/schema/types/index.js
@@ -5,4 +5,13 @@ module.exports = gql`
     id: ID!
     title: String!
   }
+  type Success {
+    success: Boolean
+    data: Todo
+  }
+  type Error {
+    success: Boolean
+    error: String
+  }
+  union Result = Success | Error
 `;
