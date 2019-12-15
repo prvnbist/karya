88e9e3c6380eaba54a3e30b2644b2891613b3ccb
Praveen Bisht <prvnbist@gmail.com>
2019-12-15 15:24:45 +0530

    feat(status): categorize todos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,16 +8,65 @@ import { Todo, AddTodo } from './components'
 // Queries
 import { GET_TODOS } from './queries'
 
+const initialState = {
+   todo: [],
+   in_progress: [],
+   done: [],
+}
+
+const reducers = (state, action) => {
+   switch (action.type) {
+      case 'TODO':
+         return {
+            ...state,
+            todo: [action.payload, ...state.todo],
+         }
+      case 'IN_PROGRESS':
+         return {
+            ...state,
+            in_progress: [action.payload, ...state.in_progress],
+         }
+      case 'DONE':
+         return {
+            ...state,
+            done: [action.payload, ...state.done],
+         }
+      default:
+         return state
+   }
+}
+
 const App = () => {
+   const [state, dispatch] = React.useReducer(reducers, initialState)
    const { loading, error, data } = useQuery(GET_TODOS)
+   React.useEffect(() => {
+      if (data?.todos) {
+         data.todos.map(todo => {
+            return dispatch({ type: todo.status, payload: todo })
+         })
+      }
+   }, [data])
 
    if (loading) return 'Loading...'
    if (error) return `Error! ${error.message}`
    return (
       <Wrapper>
          <AddTodo />
+         <h3>IN PROGRESS</h3>
          <ul>
-            {data.todos.map(todo => (
+            {state.in_progress.map(todo => (
+               <Todo key={todo.id} todo={todo} />
+            ))}
+         </ul>
+         <h3>TODO</h3>
+         <ul>
+            {state.todo.map(todo => (
+               <Todo key={todo.id} todo={todo} />
+            ))}
+         </ul>
+         <h3>DONE</h3>
+         <ul>
+            {state.done.map(todo => (
                <Todo key={todo.id} todo={todo} />
             ))}
          </ul>
@@ -32,4 +81,12 @@ const Wrapper = styled.div`
    max-width: 640px;
    padding: 0 16px;
    width: calc(100vw - 48px);
+   h3 {
+      color: #bdb5b5;
+      font-size: 14px;
+      margin-bottom: 8px;
+   }
+   ul {
+      margin-bottom: 16px;
+   }
 `
--- client/src/queries/getTodos.js
+++ client/src/queries/getTodos.js
@@ -6,6 +6,7 @@ const GET_TODOS = gql`
          id
          tags
          title
+         status
          createdAt
          updatedAt
       }
