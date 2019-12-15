adfba3c6fff2bcc4e919882742fb2ca7194af0dc
Praveen Bisht <prvnbist@gmail.com>
2019-12-15 19:39:37 +0530

    styles(status): heading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -57,19 +57,28 @@ const App = () => {
          ) : (
             <AddTodo />
          )}
-         <h3>IN PROGRESS</h3>
+         <h3 title="IN PROGRESS">
+            <span />
+            IN PROGRESS
+         </h3>
          <ul>
             {state.in_progress.map(todo => (
                <Todo key={todo.id} todo={todo} setEditMode={setEditMode} />
             ))}
          </ul>
-         <h3>TODO</h3>
+         <h3 title="TODO">
+            <span />
+            TODO
+         </h3>
          <ul>
             {state.todo.map(todo => (
                <Todo key={todo.id} todo={todo} setEditMode={setEditMode} />
             ))}
          </ul>
-         <h3>DONE</h3>
+         <h3 title="DONE">
+            <span />
+            DONE
+         </h3>
          <ul>
             {state.done.map(todo => (
                <Todo key={todo.id} todo={todo} setEditMode={setEditMode} />
@@ -86,10 +95,35 @@ const Wrapper = styled.div`
    max-width: 640px;
    padding: 0 16px;
    width: calc(100vw - 48px);
-   h3 {
-      color: #bdb5b5;
+   > h3 {
+      color: #fff;
       font-size: 14px;
+      padding: 0 10px;
+      height: 20px;
+      line-height: 20px;
       margin-bottom: 8px;
+      border-radius: 16px;
+      display: inline-block;
+      &:nth-of-type(1) {
+         background: #5da6ff;
+      }
+      &:nth-of-type(2) {
+         color: #585454;
+         background: #f5e7e7;
+      }
+      &:nth-of-type(3) {
+         background: #35da35;
+      }
+      span {
+         width: 8px;
+         height: 8px;
+         margin-right: 2px;
+         border-radius: 8px;
+         background: #fff;
+         display: inline-block;
+         transform: translate(-3px, -1px);
+         box-shadow: 1px 0 8px -0.5px rgba(0, 0, 0, 0.2);
+      }
    }
    ul {
       margin-bottom: 16px;
