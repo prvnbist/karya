193d3d984b8c82e3b950641fe0cc05cc9d6f3ccb
Praveen Bisht <prvnbist@gmail.com>
2019-12-22 22:12:38 +0530

    styles: moved to sep file

--- client/src/sections/Sidebar/index.jsx
+++ client/src/sections/Sidebar/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 import { useQuery } from '@apollo/react-hooks'
 
 // State
@@ -7,6 +6,9 @@ import { Context } from '../../context'
 
 // Logo
 import Logo from '../../assets/logo'
+
+// Styles
+import { StyledSidebar, Label, Title, NoLabels, Count } from './styles'
 
 // Assets
 import { LabelIcon } from '../../assets/icons'
@@ -57,88 +59,3 @@ const Sidebar = () => {
 }
 
 export default Sidebar
-
-const StyledSidebar = styled.div`
-   header {
-      width: 100%;
-      height: 40px;
-      margin: 16px 0;
-      display: flex;
-      align-items: center;
-      padding: 0 16px;
-   }
-   main {
-      h3 {
-         padding: 0 16px;
-         color: #bac0cf;
-         font-size: 14px;
-         letter-spacing: 0.5px;
-         text-transform: uppercase;
-         margin-bottom: 8px;
-      }
-   }
-`
-
-const NoLabels = styled.li`
-   height: 32px;
-   padding: 0 16px;
-   list-style: none;
-`
-
-const Label = styled.li`
-   display: flex;
-   align-items: center;
-   justify-content: space-between;
-   height: 32px;
-   padding: 0 16px;
-   border-radius: 4px;
-   cursor: pointer;
-   &:hover {
-      background: #f1eeff;
-      div {
-         h4 {
-            color: #6d51f4;
-         }
-         span {
-            path {
-               stroke: #6d51f4;
-            }
-            circle {
-               fill: #6d51f4;
-            }
-         }
-      }
-      > span {
-         color: #fff;
-         background: #6d51f4;
-      }
-   }
-`
-
-const Title = styled.div`
-   display: flex;
-   align-items: center;
-   height: inherit;
-   span {
-      margin-right: 12px;
-      display: flex;
-      align-items: center;
-      height: inherit;
-   }
-   h4 {
-      font-size: 16px;
-      line-height: 18px;
-      color: #8e929d;
-      font-weight: 500;
-   }
-`
-
-const Count = styled.span`
-   background: #ebedf2;
-   height: 18px;
-   padding: 0 6px;
-   border-radius: 18px;
-   font-size: 12px;
-   line-height: 18px;
-   color: #8e929d;
-`
---
+++ client/src/sections/Sidebar/styles.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components'
+
+export const StyledSidebar = styled.div`
+   header {
+      width: 100%;
+      height: 40px;
+      margin: 16px 0;
+      display: flex;
+      align-items: center;
+      padding: 0 16px;
+   }
+   main {
+      h3 {
+         color: #bac0cf;
+         font-size: 14px;
+         letter-spacing: 0.5px;
+         text-transform: uppercase;
+      }
+   }
+`
+
+export const NoLabels = styled.li`
+   height: 32px;
+   padding: 0 16px;
+   list-style: none;
+`
+
+export const Label = styled.li`
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+   height: 32px;
+   padding: 0 16px;
+   border-radius: 4px;
+   cursor: pointer;
+   &:hover {
+      background: #f1eeff;
+      div {
+         h4 {
+            color: #6d51f4;
+         }
+         span {
+            path {
+               stroke: #6d51f4;
+            }
+            circle {
+               fill: #6d51f4;
+            }
+         }
+      }
+      > span {
+         color: #fff;
+         background: #6d51f4;
+      }
+   }
+`
+
+export const Title = styled.div`
+   display: flex;
+   align-items: center;
+   height: inherit;
+   span {
+      margin-right: 12px;
+      display: flex;
+      align-items: center;
+      height: inherit;
+   }
+   h4 {
+      font-size: 16px;
+      line-height: 18px;
+      color: #8e929d;
+      font-weight: 500;
+   }
+`
+
+export const Count = styled.span`
+   background: #ebedf2;
+   height: 18px;
+   padding: 0 6px;
+   border-radius: 18px;
+   font-size: 12px;
+   line-height: 18px;
+   color: #8e929d;
+`
