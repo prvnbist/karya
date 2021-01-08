4725672de3e6fbe0f8dd47b0d66c5831a1e159cc
Praveen Bisht <prvnbist@gmail.com>
2021-01-09 03:17:36 +0530

    feat: apollo integration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+   env: {
+      HASURA_ADMIN_SECRET: process.env.HASURA_ADMIN_SECRET,
+      HASURA_HTTPS_URL: process.env.HASURA_HTTPS_URL,
+      HASURA_WSS_URL: process.env.HASURA_WSS_URL,
+   },
    webpack: (config, { isServer }) => {
       if (!isServer) {
          config.node = { fs: 'empty', module: 'empty' }
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,68 @@
+import {
+   split,
+   ApolloClient,
+   ApolloLink,
+   InMemoryCache,
+   createHttpLink,
+   ApolloProvider,
+} from '@apollo/client'
+import * as ws from 'ws'
 import { GlobalStyles } from 'twin.macro'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { WebSocketLink } from '@apollo/client/link/ws'
+
+const wssLink = process.browser
+   ? new WebSocketLink({
+        uri: process.env.HASURA_WSS_URL,
+        options: {
+           reconnect: true,
+           connectionParams: {
+              headers: {
+                 'x-hasura-admin-secret': `${process.env.HASURA_ADMIN_SECRET}`,
+              },
+           },
+        },
+     })
+   : null
+
+const authLink = new ApolloLink((operation, forward) => {
+   operation.setContext(({ headers }) => ({
+      headers: {
+         ...headers,
+         'x-hasura-admin-secret': `${process.env.HASURA_ADMIN_SECRET}`,
+      },
+   }))
+   return forward(operation)
+})
+
+const httpLink = createHttpLink({
+   uri: `${process.env.HASURA_HTTPS_URL}`,
+})
+
+const splitLink = process.browser
+   ? split(
+        ({ query }) => {
+           const definition = getMainDefinition(query)
+           return (
+              definition.kind === 'OperationDefinition' &&
+              definition.operation === 'subscription'
+           )
+        },
+        wssLink,
+        authLink.concat(httpLink)
+     )
+   : authLink.concat(httpLink)
+
+const client = new ApolloClient({
+   link: splitLink,
+   cache: new InMemoryCache(),
+})
 
 const App = ({ Component, pageProps }) => (
-   <div>
+   <ApolloProvider client={client}>
       <GlobalStyles />
       <Component {...pageProps} />
-   </div>
+   </ApolloProvider>
 )
 
 export default App
